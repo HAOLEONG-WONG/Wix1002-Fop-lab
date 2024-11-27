c0a6249a33610937e2e5256200860cb7bd62d8f1
WONG HAO LEONG <hlwong2005@gmail.com>
2024-11-27 22:55:54 +0800

    Add files via upload

---
+++ L1q3.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package l1q3;
+
+/**
+ *
+ * @author HAOLEONGWONG
+ */
+public class L1q3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    /*
+        *********        ***            *           *
+        *       *      *     *         ***         * *
+        *       *     *       *       *****       *   *
+        *       *     *       *         *        *     *
+        *       *     *       *         *       *       *
+        *       *     *       *         *        *     *
+        *       *     *       *         *         *   *
+        *       *      *     *          *          * *
+        *********        ***            *           *
+    
+    
+    
+    */
+    public static void main(String[] args) {
+        System.out.println("    *********        ***            *           *");
+        System.out.println("    *       *      *     *         ***         * *");
+        System.out.println("    *       *     *       *       *****       *   *");
+        System.out.println("    *       *     *       *         *        *     *");
+        System.out.println("    *       *     *       *         *       *       *");
+        System.out.println("    *       *     *       *         *        *     *");
+        System.out.println("    *       *     *       *         *         *   *");
+        System.out.println("    *       *      *     *          *          * *");
+        System.out.println("    *********        ***            *           *");
+        
+    }
+    
+}
